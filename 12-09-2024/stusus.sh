9bb9d586ec3c4222e03e0ec2482e0a45fefe7b85
Gabriel Christo <christogabriel547@gmail.com>
2024-09-12 11:55:40 +0700

    ini adalah studi kasus mobile prog

---
+++ 12-09-2024/stusus.sh
@@ -0,0 +1,34 @@
+function project (){
+echo "masukan jumlah pengembang  : "
+read pengembang 
+
+echo "jumlah gaji perorang untuk sebulan : "
+read gaji
+
+echo "Perancanaan berapa bulan perencangan aplikasi ini : "
+read bulan
+
+echo "biya penyewaan server : "
+read sewa_server
+
+echo "biaya lisensi softrware : "
+read lisensi_software
+
+echo "biaya operasional : "
+read operasional
+
+echo "anggaran awalan proyek yang disediakan : "
+read anggaran_yang_ada
+
+echo "--------------------------------------------"
+totalgaji=$((pengembang*gaji*bulan))
+echo "total gaji yang diperoleh pengembang adalah $totalgaji"
+
+totalbiaya=$(((sewa_server*bulan)+operasional+lisensi_software))
+echo "total biaya sewa server dan biaya operasional adalah $totalbiaya"
+
+hitungangajidanbiayadananggaran=$((anggaran_yang_ada-(totalgaji+totalbiaya)))
+echo "total hitungan semuanya adalah $hitungangajidanbiayadananggaran"
+
+}
+project
